6ba77ac856c8442415c2884b253b773dd4e752b6
Sergey86fr <sergsfree@gmail.com>
2026-05-25 15:28:44 +0500

    create env

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://socnet-backend.onrender.com' : 'http://localhost:3000' 
+export const BASE_URL = import.meta.env.VITE_API_URL as string;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+type ImportMetaEnv = {
+  readonly VITE_API_URL: string;
+  // добавьте другие переменные по мере необходимости
+}
+
+type ImportMeta = {
+  readonly env: ImportMetaEnv;
+}
